153d42cef2241cbdbdb646ba450793f7daa9f9d7
Miguel Lorenzo V Dungan <miguel_dungan@dlsu.edu.ph>
2021-05-14 17:56:38 +0800

    h screen added

--- next/pages/roadmap.js
+++ next/pages/roadmap.js
@@ -3,7 +3,7 @@ import { directories } from '../datas/directory';
 const Home = () => {
 
   return (
-    <div className="fourth-page" >
+    <div className="fourth-page h-screen"  >
       <div className="p4-foreground" >
         <img src="/images/beads_words.png" />
       </div>
