8936f1ff5c1c0867d1742895c640199a9b00038e
THEREALHELIAS <helias@DESKTOP-POGPB3M.localdomain>
2021-05-14 14:13:16 +0800

    Added Nav bar

---
+++ next/datas/directory.js
@@ -0,0 +1,30 @@
+export const directories = [
+  {
+    name: 'sextoy shop',
+    link: ''
+  },
+  {
+    name: 'about',
+    link: ''
+  },
+  {
+    name: 'tokenomics',
+    link: ''
+  },
+  {
+    name: 'whitepaper',
+    link: ''
+  },
+  {
+    name: 'roadmap',
+    link: ''
+  },
+  {
+    name: 'bscscan',
+    link: ''
+  },
+  {
+    name: 'buy pancake',
+    link: ''
+  }
+]
--- next/pages/index.js
+++ next/pages/index.js
@@ -1,7 +1,27 @@
+import {directories} from '../datas/directory';
+
 const Home = () => {
-  return (<div>
-    New Crypto page
-  </div>)
+
+  return (
+    <div className="root">
+      <nav className="p-5 flex flex-row justify-between">
+        <div className="max-w-logo-max">
+          <img src="/sextoy.png"/>
+        </div>
+        <ul className="flex flex-row">
+          { directories.map(item => (
+            <li className='px-2' key={item.name}>
+              <a>
+                <span className="capitalize text-xl text-red-500">
+                  {item.name}
+                </span>
+              </a>
+            </li>
+          )) }
+        </ul>
+      </nav>
+    </div>  
+  )
 }
 
 export default Home;
