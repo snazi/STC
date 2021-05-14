35b212689c0d515b099ee531623cddbff827af2b
Kurt Neil Aquino <kurt_aquino@dlsu.edu.ph>
2021-05-14 16:08:02 +0800

    Quartzo Font

--- next/tailwind.config.js
+++ next/tailwind.config.js
@@ -5,7 +5,17 @@ module.exports = {
     extend: {
       maxWidth: {
         'logo-max': '11rem'
+      },
+      backgroundImage: theme => ({
+        'bg1': src="/images/bg1.png",
+        'bg2': src="/images/bg2.png",
+        'bg3': src="/images/bg3.png",
+        'bg4': src="/images/bg4.png",
+      }),
+      fontFamily: {
+        'sans': ['QuartzoBold']
       }
+
     },
   },
   variants: {
